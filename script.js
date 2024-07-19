4a128e9e3377bb0ed89979ab4f22d373b72b3741
shaloojais8876 <104994730+shaloojais8876@users.noreply.github.com>
2024-07-19 19:56:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Complete the js code
-function Car(make, model) {}
-
-function SportsCar(make, model, topSpeed) {}
-
+class Car{
+	constructor(make,model,){
+		this.make = make;
+		this.model = model;
+	}
+	getMakeModel(){
+		return "Scorpio", 2001;
+	}
+}
+class SportsCar{
+	constructor(make,model,topSpeed){
+		this.make = make;
+		this.model = model;
+		this.topSpeed = topSpeed;
+	}
+	getTopSpeed(){
+		return "bmw", 2000,40;
+	}
+}
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
